4037effc194bb605457a8094bb7d1678e891eddd
Nico de Wet <nico@nicodewet.com>
2015-09-02 20:02:39 +1200

    adding thymeleaf, jquery, bootstrap, devtools, actuator and borrowing from https://github.com/kolorobot/spring-boot-thymeleaf

---
+++ src/main/java/hello/HomeController.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class HomeController {
+
+    @RequestMapping("/")
+    String index() {
+        return "index";
+    }
+
+    @RequestMapping("properties")
+    @ResponseBody
+    java.util.Properties properties() {
+        return System.getProperties();
+    }
+}
