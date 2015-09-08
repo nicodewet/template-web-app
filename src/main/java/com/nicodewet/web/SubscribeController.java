b1c09a395615395205f9df6e164b852afbe4a6fc
Nico de Wet <nico@nicodewet.com>
2015-09-08 16:29:32 +1200

    adding subscribeth.json RequestMapping in SubscribeController, temporarily doing javascript based submit, adding generic validation classes for JSR 303 based form validation

--- src/main/java/com/nicodewet/web/SubscribeController.java
+++ src/main/java/com/nicodewet/web/SubscribeController.java
@@ -19,15 +19,24 @@
  */
 package com.nicodewet.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.nicodewet.domain.Subscription;
 import com.nicodewet.domain.SubscriptionType;
+import com.nicodewet.web.validation.ErrorMessage;
+import com.nicodewet.web.validation.ValidationResponse;
 
 @Controller
 public class SubscribeController {
@@ -48,15 +57,33 @@ public class SubscribeController {
         return "subscribeth";
     }
 
-    @RequestMapping(value="/subscribeth", params={"save"})
+    @RequestMapping(value="/subscribeth", method=RequestMethod.POST)
     public String subscribe(@Valid final Subscription subscription, final BindingResult bindingResult, final ModelMap model) {
-        if (bindingResult.hasErrors()) {
+    	if (bindingResult.hasErrors()) {
             return "subscribeth";
         } else {        	
         	log.info("JUST ADDED SUBSCRIPTION: " + subscription);
         	model.clear();
         	return "redirect:/subscribeth";
         }
+    }
+    
+    @RequestMapping(value="/subscribeth.json", method=RequestMethod.POST)
+    public @ResponseBody ValidationResponse processForm(@Valid final Subscription subscription, final BindingResult bindingResult, final ModelMap model) {
+    	log.info("Validation");
+    	ValidationResponse res = new ValidationResponse();
+    	if (!bindingResult.hasErrors()) {
+    		res.setStatus("SUCCESS");
+    	} else {
+    		res.setStatus("FAIL");
+    		List<FieldError> allErrors = bindingResult.getFieldErrors();
+			List<ErrorMessage> errorMesages = new ArrayList<ErrorMessage>();
+			for (FieldError objectError : allErrors) {
+				errorMesages.add(new ErrorMessage(objectError.getField(), objectError.getField() + "  " + objectError.getDefaultMessage()));
+			}
+			res.setErrorMessageList(errorMesages);
+    	}
+    	return res;
     }
 
 }
---
+++ src/main/java/com/nicodewet/web/validation/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * See <a href="http://spring.io/blog/2012/08/29/integrating-spring-mvc-with-jquery-for-validation-rules/">Integrating Spring MVC with jQuery for validation rules</a>
+ */
+package com.nicodewet.web.validation;
