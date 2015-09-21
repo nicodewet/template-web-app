d029dc9b6639ca1c448ff42dd0707c97e7211909
Nico de Wet <nico@nicodewet.com>
2015-09-22 10:23:50 +1200

    adding etc dir with eclipse-cleanup-profile.xml, eclipse-java-google-style.xml and README.txt with reminder to add Save Actions

---
+++ etc/README.txt
@@ -0,0 +1,12 @@
+== Eclipse ==
+
+To get Eclipse to format and cleanup code every time you save, remember to enable and test your Save Actions.
+
+To do the following:
+
+1. Go to Window > Preferences > Java > Editor > Save Actions.
+2. Select Perform the selected actions on save.
+3. Select Format Source code. ...
+4. Make sure Organize imports is selected.
+5. Select Additional actions.
+6. Click Ok, edit some code, save it and watch Eclipse format it automatically.
--- src/main/java/com/nicodewet/domain/Subscription.java
+++ src/main/java/com/nicodewet/domain/Subscription.java
@@ -1,19 +1,17 @@
 /*
  * =============================================================================
  * 
- *   Copyright (c) 2011, The THYMELEAF team (http://www.thymeleaf.org)
+ * Copyright (c) 2011, The THYMELEAF team (http://www.thymeleaf.org)
  * 
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  * 
  * =============================================================================
  */
@@ -24,54 +22,54 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 public class Subscription {
 
-	private String firstname;
-	private String surname;
-	private String email;
-    private SubscriptionType subscriptionType = SubscriptionType.ALL_EMAILS;
-    
-    public Subscription() {
-        super();
-    }
+  private String firstname;
+  private String surname;
+  private String email;
+  private SubscriptionType subscriptionType = SubscriptionType.ALL_EMAILS;
 
-    @NotEmpty
-    @Email
-    public String getEmail() {
-        return this.email;
-    }
+  public Subscription() {
+    super();
+  }
 
-    public void setEmail(final String email) {
-        this.email = email;
-    }
-    
-    @NotEmpty
-    public String getFirstname() {
-		return firstname;
-	}
+  @NotEmpty
+  @Email
+  public String getEmail() {
+    return this.email;
+  }
 
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
+  public void setEmail(final String email) {
+    this.email = email;
+  }
 
-	@NotEmpty
-	public String getSurname() {
-		return surname;
-	}
+  @NotEmpty
+  public String getFirstname() {
+    return firstname;
+  }
 
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
 
-    public SubscriptionType getSubscriptionType() {
-        return this.subscriptionType;
-    }
+  @NotEmpty
+  public String getSurname() {
+    return surname;
+  }
 
-    public void setSubscriptionType(final SubscriptionType subscriptionType) {
-        this.subscriptionType = subscriptionType;
-    }
-    
-	@Override
-	public String toString() {
-		return "Subscription [firstname=" + firstname + ", surname=" + surname + ", email=" + email
-				+ ", subscriptionType=" + subscriptionType + "]";
-	}
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
+
+  public SubscriptionType getSubscriptionType() {
+    return this.subscriptionType;
+  }
+
+  public void setSubscriptionType(final SubscriptionType subscriptionType) {
+    this.subscriptionType = subscriptionType;
+  }
+
+  @Override
+  public String toString() {
+    return "Subscription [firstname=" + firstname + ", surname=" + surname + ", email=" + email
+        + ", subscriptionType=" + subscriptionType + "]";
+  }
 }
